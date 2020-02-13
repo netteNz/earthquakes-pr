c08a8bc870320d0037b72aae95b95f742e7e7a8b
Emanuel Lugo <lugo.emanuel@gmail.com>
2020-02-12 22:21:50 -0400

    ok

--- earthquakes-pr/desktop_temp.py
+++ earthquakes-pr/desktop_temp.py
@@ -4,18 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-pr = folium.Map(location=[18.2208, -66.5901],
-                zoom_start=9,
-                tiles='cartodbdark_matter'
-               )
 
-df = pd.read_csv('query45.csv',
-                 usecols=['latitude', 'longitude','place', 'mag'],
+pr = folium.Map(location=[18.2208, -66.5901],
+        zoom_start=9,
+        tiles='cartodbdark_matter'
+        )
+
+
+df = pd.read_csv('query45.csv', 
+                usecols=['latitude', 'longitude','place', 'mag'],
                 )
 
-lat = []
-lon = []
 
-for index, row in df.iterrows():
-    lat.append(row['latitude']), lon.append(row['longitude'])
-    print(lat,lon)
+def plot(point):
+    lat = []
+    lon = []
+
+    for i, row in df.iterrows():
+        lat.append(row[i]['latitude']), lon.append(row[i]['longitude'])
+        folium.CircleMarker(location=[point.lat, point.lon], radius=2).add_to(pr)
